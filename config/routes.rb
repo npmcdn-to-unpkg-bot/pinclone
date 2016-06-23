6c407a289a223fe5ce4e5ed32c1cddbea733493a
gwilson09 <gwilson09@gmail.com>
2016-06-23 14:37:25 -0700

    working

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   end
   root to: redirect('/users/sign_in')
 
+  get ':username' => 'users#show', as: 'user'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
