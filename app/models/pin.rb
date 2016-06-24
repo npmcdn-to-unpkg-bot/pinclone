59c1242238d598232f9871bec64e129fbe0d3aa3
gwilson09 <gwilson09@gmail.com>
2016-06-23 17:13:17 -0700

    working now

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,14 @@
 class Pin < ActiveRecord::Base
+  validates_presence_of :description
   belongs_to :user
   has_attached_file :pin_image, styles: { medium: "300x300>", thumb: "100x100>" }
-  #validates_attachment_content_type :pin_image, :content_type => ["image/jpeg", "image/gif", "image/png"]
   validates_attachment_content_type :pin_image, content_type: /\Aimage\/.*\Z/
-  # validates_attachment_content_type :pin_image, :content_type => [/\Aimage/, 'application/octet-stream']
-  #do_not_validate_attachment_file_type :pin_image
+
+  def repost(user)
+    repost_pin = self.dup
+    repost_pin.user_id = user.id
+    repost_pin.original_pin_id = self.id
+    repost_pin.save
+  end
 
 end
